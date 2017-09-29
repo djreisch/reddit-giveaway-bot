e4a1af9b0f3ebbe44d29e24c0a62b35d3d5d11fc
Daniel Reisch <danieljreisch@gmail.com>
2017-09-29 15:09:40 -0700

    added strings file

---
+++ strings.py
@@ -0,0 +1,75 @@
+
+# The title and body of self post created for the giveaway.
+# Only relevant if the -r or --reddit options are provided
+# on the command line.
+submission_title = """Random Steam Key Giveaway! {keys} Keys Available to Win!"""
+
+submission_body = """Comment on this post to win a prize."""
+
+
+# A message appended to the submission_body when a keyword is
+# required to signal the bot for a giveaway (-k/--keyword).
+# All occurances of {keyword} will be replaced with the
+# keyword (leading and trailing whitespace will be stripped).
+keyword_message = """To filter out chatter, the word
+`{keyword}` must be present in your comment for you to be
+eligible to win a prize."""
+
+
+# If prizes are distributed by submission time (the default),
+# this message will be appended to the submission_body to
+# let users know how prizes will be distributed.
+timestamp_rule = """Prizes will be given out on a first-come
+first-serve basis, comment now before all the prizes are gone!."""
+
+# If prizes are distributed randomly, this message will be
+# appended to the submission_body to let users know
+# how prizes will be distributed.
+# Parameters:
+#   {wait}: Replaced with the wait time, humanized (eg. "30 minutes")
+#   {utc}: Replaced with the exact UTC time that the bot will begin
+#     distributing prizes.
+random_rule = """Prizes will be given out randomly after a
+period of {wait} minutes (at {utc}). There is no guarantee that prizes
+will be available once that time passes."""
+
+
+what_is_this = """"""
+#what_is_this = """This contest is being managed by a bot.
+#To get him to cater at your next giveaway, please visit the
+#[Github page](https://github.com/t-r-a-g-e-d-y/reddit-giveaway-bot)!"""
+
+
+# The title of the pm that winners will receive.
+# This will not be used if the bot replies inline with the prizes.
+reply_title = """Giveaway Prize"""
+
+# The contents of the reply/pm that winners will receive.
+# Message is formatted via Reddit's Markup.
+# All occurances of {prize} will be replaced with the prize value
+# and occurances of {url} will be replaced with the post url.
+# '{' and '}' can be entered into the message by doubling
+# them up ({{ and }}).
+prize_reply_message = """Congratulations, you have been a randomly
+selected as a winner for the giveaway you entered! Your prize is below:
+
+Steam Key: {prize}
+
+[What Contest?]({url})
+
+---
+
+^I ^am ^a ^bot, ^this ^action ^was ^performed ^automatically."""
+
+generic_reply_message = """Congratulations, you have been picked as one
+of the winners!
+
+Check your inbox for your prize.
+
+---
+
+^I ^am ^a ^bot, ^this ^action ^was ^performed ^automatically."""
+
+# A message edited onto the end of the post once
+# bot has run out of prizes to give out.
+end_message = "I'm all out of prizes! Thanks for playing."
