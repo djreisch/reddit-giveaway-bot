f306a3677887b65e5df3eff6f7f7ed9a0f12adb0
Daniel Reisch <danieljreisch@gmail.com>
2017-10-03 10:25:35 -0700

    updated args file

--- argsfile.py
+++ argsfile.py
@@ -1,12 +1,10 @@
-age=''
-poll=''
-keyword=''
-reply=''
-random=''
-wait=''
-submission=''
-reddit=''
-keyfile=''
+age= 1
+poll= 30
+reply= 'pm'
+random= True
+karmaLink = 0
+karmaComment = 0
+
 pcmr_flair_open='58bb117a-c51a-11e3-b96c-12313d056a4d'
 pcmr_flair_closed='602bec40-c51a-11e3-a48b-12313d1870c3'
 sg_flair_open='03a30d2c-034e-11e6-ac67-0e43b37a78c1'
